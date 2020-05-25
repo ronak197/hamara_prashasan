f6113c9b82ae571c3399b4a96426ead0df361f42
Harsh Lukka <43118306+hadsluks@users.noreply.github.com>
2020-05-25 21:14:48 +0530

    Changed UI of send_post_page

--- lib/classes.dart
+++ lib/classes.dart
@@ -1,25 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-Map<String,String> avatarLocMap = {
-  'health' : 'assets/health_avatar.svg',
-  'police' : 'assets/police_avatar.svg',
-  'muncorp' : 'assets/muncorp_avatar.svg',
+Map<String, String> avatarLocMap = {
+  'health': 'assets/health_avatar.svg',
+  'police': 'assets/police_avatar.svg',
+  'muncorp': 'assets/muncorp_avatar.svg',
 };
 
-Map<String,int> avatarColorMap = {
-  'health' : 0xffFFEEED,
-  'police' : 0xffFFFCED,
-  'muncorp' : 0xffF1EDFF,
+Map<String, int> avatarColorMap = {
+  'health': 0xffFFEEED,
+  'police': 0xffFFFCED,
+  'muncorp': 0xffF1EDFF,
 };
 
-class Feed{
+class Feed {
   FeedInfo feedInfo;
   Department department;
   FeedInfoDetails feedInfoDetails;
   String profileAvatar;
   int bgColor;
 
-  Feed(FeedInfo feedInfo, Department department, FeedInfoDetails feedInfoDetails){
+  Feed(FeedInfo feedInfo, Department department,
+      FeedInfoDetails feedInfoDetails) {
     this.feedInfo = feedInfo;
     this.department = department;
     this.feedInfoDetails = feedInfoDetails;
@@ -27,54 +28,52 @@ class Feed{
     this.bgColor = avatarColorMap['${department.category}'];
   }
 }
-class FeedInfo {
 
+class FeedInfo {
   DateTime creationDateTimeStamp;
   String departmentUid;
   String description;
   String title;
 
-  FeedInfo({
-    this.creationDateTimeStamp,
-    this.departmentUid,
-    this.description,
-    this.title
-  });
+  FeedInfo(
+      {this.creationDateTimeStamp,
+      this.departmentUid,
+      this.description,
+      this.title});
 
   factory FeedInfo.fromJson(Map<String, dynamic> json) => FeedInfo(
-      creationDateTimeStamp : json["creationDateTimeStamp"],
-      departmentUid : json["departmentUid"],
-      description : json["description"],
-      title : json["title"],
-  );
+        creationDateTimeStamp: json["creationDateTimeStamp"],
+        departmentUid: json["departmentUid"],
+        description: json["description"],
+        title: json["title"],
+      );
 
   Map<String, dynamic> toJson() => {
-    "creationDateTimeStamp" : creationDateTimeStamp,
-    "departmentUid" : departmentUid,
-    "description" : description,
-    "title" : title,
-  };
+        "creationDateTimeStamp": creationDateTimeStamp,
+        "departmentUid": departmentUid,
+        "description": description,
+        "title": title,
+      };
 }
 
 class FeedInfoDetails {
-
-  List<Map<String,dynamic>> details;
+  List<Map<String, dynamic>> details;
 
   FeedInfoDetails({
     this.details,
   });
 
-  factory FeedInfoDetails.fromJson(Map<String, dynamic> json) => FeedInfoDetails(
-    details: List<Map<String,dynamic>>.from(json["details"].map((x) => x)),
-  );
+  factory FeedInfoDetails.fromJson(Map<String, dynamic> json) =>
+      FeedInfoDetails(
+        details: List<Map<String, dynamic>>.from(json["details"].map((x) => x)),
+      );
 
   Map<String, dynamic> toJson() => {
-    "details": List<Map<String,dynamic>>.from(details.map((x) => x)),
-  };
+        "details": List<Map<String, dynamic>>.from(details.map((x) => x)),
+      };
 }
 
 class User {
-
   List<String> bookmarkedFeeds;
   String email;
   DateTime lastFeedUpdateTime;
@@ -82,61 +81,66 @@ class User {
   List<String> subscribedDepartmentIDs;
   String userType;
 
-
-  User({this.bookmarkedFeeds, this.email, this.lastFeedUpdateTime,
-      this.lastLocation, this.subscribedDepartmentIDs, this.userType});
+  User(
+      {this.bookmarkedFeeds,
+      this.email,
+      this.lastFeedUpdateTime,
+      this.lastLocation,
+      this.subscribedDepartmentIDs,
+      this.userType});
 
   factory User.fromJson(Map<String, dynamic> json) => User(
-      bookmarkedFeeds : json["bookmarkedFeeds"],
-      email : json["email"],
-      lastFeedUpdateTime : json["lastFeedUpdateTime"],
-      lastLocation : json["lastLocation"],
-      subscribedDepartmentIDs : json["subscribedDepartmentIDs"],
-      userType : json["userType"],
-  );
+        bookmarkedFeeds: json["bookmarkedFeeds"],
+        email: json["email"],
+        lastFeedUpdateTime: json["lastFeedUpdateTime"],
+        lastLocation: json["lastLocation"],
+        subscribedDepartmentIDs: json["subscribedDepartmentIDs"],
+        userType: json["userType"],
+      );
 
   Map<String, dynamic> toJson() => {
-    "bookmarkedFeeds" : bookmarkedFeeds,
-    "email" : email,
-    "lastFeedUpdateTime" : lastFeedUpdateTime,
-    "lastLocation" : lastLocation,
-    "subscribedDepartmentIDs" : subscribedDepartmentIDs,
-    "userType" : userType,
-  };
+        "bookmarkedFeeds": bookmarkedFeeds,
+        "email": email,
+        "lastFeedUpdateTime": lastFeedUpdateTime,
+        "lastLocation": lastLocation,
+        "subscribedDepartmentIDs": subscribedDepartmentIDs,
+        "userType": userType,
+      };
 }
 
 class Department {
-
   String areaOfAdministration;
   String category;
   String email;
   String name;
   String userType;
 
-  Department({this.areaOfAdministration, this.category, this.email,
-      this.name, this.userType});
+  Department(
+      {this.areaOfAdministration,
+      this.category,
+      this.email,
+      this.name,
+      this.userType});
 
   factory Department.fromJson(Map<String, dynamic> json) => Department(
-      areaOfAdministration : json["areaOfAdministration"],
-      category : json["category"],
-      email : json["email"],
-      name : json["name"],
-      userType : json["userType"],
-  );
+        areaOfAdministration: json["areaOfAdministration"],
+        category: json["category"],
+        email: json["email"],
+        name: json["name"],
+        userType: json["userType"],
+      );
 
   Map<String, dynamic> toJson() => {
-    "areaOfAdministration" : areaOfAdministration,
-    "category" : category,
-    "email" : email,
-    "name" : name,
-    "userType" : userType,
-  };
+        "areaOfAdministration": areaOfAdministration,
+        "category": category,
+        "email": email,
+        "name": name,
+        "userType": userType,
+      };
 }
 
 class TableData {
   List<String> headers;
   List<List<String>> contents;
-  TableData({this.headers,this.contents})
-      : assert(contents.length == 0 ||
-            (contents.length > 0 && headers.length == contents[0].length));
+  TableData({this.headers, this.contents});
 }
---
+++ lib/searchPlace.dart
@@ -0,0 +1,238 @@
+import 'dart:convert';
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
+
+class SearchResult {
+  String searchString, name, address;
+  LatLng pos;
+  SearchResult(
+      {@required this.name,
+      @required this.pos,
+      @required this.searchString,
+      @required this.address});
+}
+
+class SearchPlace extends StatefulWidget {
+  final Function addPlaces;
+  SearchPlace({@required this.addPlaces});
+  @override
+  _SearchPlaceState createState() => _SearchPlaceState();
+}
+
+class _SearchPlaceState extends State<SearchPlace> {
+  final apiKey = "AIzaSyA4ZokZx9JRs-T1z0EpZxzqHVKlZ-sZkC0";
+  Completer<GoogleMapController> _controller = Completer();
+  static final CameraPosition _myLocation =
+      CameraPosition(target: LatLng(0, 0));
+  List<SearchResult> allResults = [], currentResults = [];
+  double searchCode;
+  TextEditingController _searchcontroller;
+  Set<Marker> places = new Set<Marker>();
+  bool showSuggestions = false;
+
+  void searchPlaces(String s) async {
+    double mysearchCode = Random().nextDouble();
+    searchCode = mysearchCode;
+    await Future.delayed(Duration(milliseconds: 1000));
+    if (mysearchCode == searchCode) {
+      s = s.replaceAll(" ", "%20");
+      String url =
+          "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=$apiKey&input=$s&inputtype=textquery&fields=name,geometry,formatted_address";
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        var data = json.decode(response.body);
+        currentResults.clear();
+        for (var loc in data['candidates']) {
+          var coord = loc['geometry']['location'];
+          SearchResult sr = new SearchResult(
+              name: loc['name'],
+              pos: new LatLng(coord['lat'], coord['lng']),
+              searchString: s,
+              address: loc['formatted_address']);
+          currentResults.add(sr);
+        }
+        showSuggestions = true;
+        setState(() {});
+      }
+      print(currentResults);
+    }
+  }
+  /*{candidates: [{formatted_address: Opp Punjab National Bank, Bangdi bazar, Kedareshwar Rd, Sudama Puri, Porbandar, Gujarat 360575, India,
+   geometry: {location: {lat: 21.6415157, lng: 69.6029045}, viewport: {northeast: {lat: 21.64276137989272, lng: 69.60424282989273},
+    southwest: {lat: 21.64006172010728, lng: 69.60154317010728}}}, name: Sweta Novelty}], status: OK} */
+
+  void onTapLocation(int index) {
+    var place = currentResults[index];
+    showSuggestions = false;
+    places.add(
+      new Marker(
+        markerId: new MarkerId(place.name),
+        position: place.pos,
+        consumeTapEvents: true,
+      ),
+    );
+    _controller.future.then(
+      (mapController) => mapController.animateCamera(
+        CameraUpdate.newLatLngZoom(place.pos, 12.0),
+      ),
+    );
+    setState(() {});
+  }
+
+  void onTapMarker(Marker m) {
+    setState(() {
+      m = m.copyWith(
+          iconParam:
+              BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen));
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _searchcontroller = new TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double siggestionsHeight = min(currentResults.length * 80.0, 200.0) + 20;
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
+        iconTheme: IconThemeData(color: Colors.black),
+        titleSpacing: 0.0,
+        backgroundColor: Colors.white,
+        title: TextField(
+          controller: _searchcontroller,
+          keyboardType: TextInputType.text,
+          textInputAction: TextInputAction.search,
+          onTap: () {
+            setState(() {
+              showSuggestions = true;
+            });
+          },
+          onChanged: searchPlaces,
+          onSubmitted: searchPlaces,
+          style: Theme.of(context)
+              .textTheme
+              .headline2
+              .copyWith(color: Colors.black),
+          cursorColor: Colors.black54,
+          minLines: 1,
+          maxLines: 1,
+          maxLength: 100,
+          maxLengthEnforced: true,
+          enabled: true,
+          decoration: InputDecoration(
+            hintText: 'Search Place...',
+            hintStyle: Theme.of(context)
+                .textTheme
+                .headline4
+                .copyWith(color: Colors.grey[600]),
+            counterText: "",
+            contentPadding: EdgeInsets.all(12.0),
+            floatingLabelBehavior: FloatingLabelBehavior.auto,
+            border: InputBorder.none,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.clear),
+            onPressed: () {
+              currentResults.clear();
+              _searchcontroller.clear();
+              setState(() {});
+            },
+          ),
+        ],
+        bottom: showSuggestions && currentResults.length > 0
+            ? PreferredSize(
+                child: Container(
+                  height: siggestionsHeight,
+                  child: ListView(
+                    physics: BouncingScrollPhysics(),
+                    children: new List<Widget>.generate(
+                      currentResults.length,
+                      (index) {
+                        var place = currentResults[index];
+                        return ListTile(
+                          onTap: () {
+                            onTapLocation(index);
+                          },
+                          title: Text(
+                            place.name,
+                            style: Theme.of(context).textTheme.headline2,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          subtitle: Text(
+                            place.address,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline1
+                                .copyWith(color: Colors.grey),
+                          ),
+                          trailing: Icon(Icons.location_searching),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+                preferredSize: Size.fromHeight(siggestionsHeight),
+              )
+            : null,
+      ),
+      floatingActionButton: places.length > 0
+          ? RaisedButton(
+              onPressed: () {
+                Navigator.pop(context);
+                widget.addPlaces(places.toList());
+              },
+              color: Color(0xff1010fc),
+              elevation: 20,
+              padding: EdgeInsets.all(15),
+              child: Text(
+                places.length > 1 ? "Add all Places" : "Add Place",
+                style: Theme.of(context)
+                    .textTheme
+                    .headline3
+                    .copyWith(color: Colors.white),
+              ),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)),
+            )
+          : null,
+      body: GestureDetector(
+        onTap: () {
+          /* if (!FocusScope.of(context).hasPrimaryFocus)
+            FocusScope.of(context).unfocus(); */
+        },
+        child: GoogleMap(
+          initialCameraPosition: _myLocation,
+          mapType: MapType.normal,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+          myLocationButtonEnabled: true,
+          myLocationEnabled: true,
+          markers: places,
+          onTap: (pos) {
+            var marker;
+            try {
+              marker = places.firstWhere((marker) => marker.position == pos);
+            } catch (e) {
+              print(e);
+            }
+            print(marker);
+            if (marker != null) onTapMarker(marker);
+          },
+        ),
+      ),
+    );
+  }
+}
